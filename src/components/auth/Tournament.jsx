41d5fa3fd8197d9760437467b10f37955b20f824
ManavSharma333 <mshar246@my.centennialcollege.ca>
2023-12-12 14:59:10 -0500

    layout

--- src/components/auth/Tournament.jsx
+++ src/components/auth/Tournament.jsx
@@ -29,19 +29,22 @@ const TournamentPage = () => {
     <div style={pageStyle}>
       <h2>Tournament Details</h2>
       <div className="tournament-info" style={infoStyle}>
-        <h3>Tournament Name: Example Tournament</h3>
+        <h3>Tournament Name: Basketball Premier League (BPL)</h3>
         <p>Description: This tournament features the best players in the league...</p>
+        <p>1) Mitchel Marsh</p>
+        <p>2)Smith</p>
+        <p>3) Josh Root</p>
         {/* Other tournament details */}
       </div>
 
       <div className="tournament-matches">
         <h3>Matches</h3>
         <div className="match-card" style={matchCardStyle}>
-          <p>Match 1: Player 1 vs. Player 2</p>
+          <p>Match 1: Josh Root vs. Rashid</p>
           {/* Display match details */}
         </div>
         <div className="match-card" style={matchCardStyle}>
-          <p>Match 2: Player 3 vs. Player 4</p>
+          <p>Match 2: Smith vs. Mitchel Marsh</p>
           {/* Display match details */}
         </div>
         {/* Other matches */}
@@ -50,11 +53,25 @@ const TournamentPage = () => {
       <div className="tournament-results" style={resultCardStyle}>
         <h3>Tournament Results</h3>
         <div className="result-card">
-          <p>Winner: Player X</p>
+          <p>Winner: Smith</p>
           {/* Display result details */}
         </div>
         {/* Other result cards */}
       </div>
+      <footer>
+  <div class="container">
+    <div class="footer-content">
+      <div class="footer-section" >
+        <h3>Contact Us</h3>
+        <p>Email: info@example.com</p>
+        <p> Phone: (555) 123-4567</p>
+      </div>
+    </div>
+    <div class="footer-bottom">
+      <p>&copy; 2023 Binary Beats. All rights reserved. | <a href="#">Privacy Policy</a> | <a href="#">Terms of Service</a></p>
+    </div>
+  </div>
+</footer>
     </div>
   );
 };
